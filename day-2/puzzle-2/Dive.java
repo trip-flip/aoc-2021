12c14100ef51c6d94b83f0fd60a154d55fb1c507
trip-flip <thomasbaugher@outlook.com>
2021-12-02 11:09:08 -0500

    Added puzzle-2

---
+++ day-2/puzzle-2/Dive.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Dive {
+    public static void main(String[] args) {
+        if (args.length == 0) return;
+        int depth = 0, horizontal = 0;
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(args[0]));
+            ArrayList<Command> commands = new ArrayList<>();
+            String read;
+            while ((read = bf.readLine()) != null) {
+                commands.add(new Command(read));
+            }
+
+            for (Command c : commands) {
+                CommandType type = c.getCommandType();
+                if (type == CommandType.Forward) {
+                    horizontal += c.getValue();
+                } else {
+                    depth += c.getValue();
+                }
+            }
+
+            final int result = horizontal * depth;
+            System.out.println("Position and depth (horizontal * depth): " + result);
+
+            bf.close();
+        } catch (IOException e) {
+            System.err.print(e);
+        } 
+    }
+}
